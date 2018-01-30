c3434913a4c2cfd559ac3809829173dd5f107150
Klaiber Stefan (INST-IOT/ESW-Wa) <Stefan.Klaiber2@bosch-si.com>
2018-01-30 16:41:36 +0100

    Implement introduction screen.

--- logic.js
+++ logic.js
@@ -12,5 +12,14 @@ function changeView() {
         intro.style.display = "block";
         tit.textContent = "Introduction";
     }
+}
+
+function openPopup() {
+    var test = document.getElementById("dialogAgri");
+    if(test.style.display === "none"){
+        test.style.display = "block";
+    }else {
+        test.style.display = "none";
+    }
 
 }
