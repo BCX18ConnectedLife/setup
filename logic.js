22e9a118e416d2101e3de3b4261c10c622e1443b
KLS1WA3 <Stefan.Klaiber2@bosch-si.com>
2018-02-13 09:04:52 +0100

    Update new setup.png Add first draft of button.

--- logic.js
+++ logic.js
@@ -2,14 +2,18 @@ function changeView() {
     var api = document.getElementById("api");
     var intro = document.getElementById("intro");
     var tit = document.getElementById("titleText");
+    var btn = document.getElementById("viewButton");
+
 
     if (api.style.display == "none") {
         api.style.display = "block";
         intro.style.display = "none";
         tit.textContent = "Setup";
+        btn.textContent = "Introduction";
     } else {
         api.style.display = "none";
         intro.style.display = "block";
         tit.textContent = "Introduction";
+        btn.textContent = "Setup";
     }
 }
